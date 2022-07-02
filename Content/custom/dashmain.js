6af40dde269ae483d64b0b0e8ba14f02259aaf17
winfogram <winfogram@gmail.com>
2022-07-02 17:59:44 +0530

    Now Customer Report is working fine

--- Content/custom/dashmain.js
+++ Content/custom/dashmain.js
@@ -1110,6 +1110,33 @@ $(document).ready(function () {
         });
     }).draw();
 
+    var tblporeport = $('#tblporeport').DataTable({
+        "scrollX": true,
+        "paging": false,
+        "info": false,
+        "dom": 'Bfrtip',
+        "buttons": [
+            'pageLength',
+            {
+                extend: 'excel',
+                text: '<i class="far fa-file-excel"></i> Excel',
+                exportOptions: {
+                    columns: ':visible'
+                },
+                footer: true
+            },
+            'colvis'
+        ]
+    });
+
+    tblporeport.on('order.dt search.dt', function () {
+        tblporeport.column(0, { search: 'applied', order: 'applied' }).nodes().each(function (cell, i) {
+            cell.innerHTML = i + 1;
+            tblporeport.cell(cell).invalidate('dom');
+        });
+    }).draw();
+    //Registered Vendor Index Info Table
+
     //Registered Vendor Index Info Table
     var tblpurchaseorder = $('#tblpurchaseorder').DataTable({
         "columnDefs": [
